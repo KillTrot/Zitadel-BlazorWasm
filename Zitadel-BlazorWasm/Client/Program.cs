33b54e4d4fd32a234b03ebd7382b7ba513c90b6e
Cedric <cedric-kell@gmx.ch>
2023-05-01 10:12:27 +0200

    Projektdateien hinzufügen.

---
+++ Zitadel-BlazorWasm/Client/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Zitadel_BlazorWasm;
+
+namespace Zitadel_BlazorWasm
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                builder.Configuration.Bind("Zitadel", options.ProviderOptions);
+                options.ProviderOptions.ResponseType = "code";
+                options.ProviderOptions.ResponseMode = "query";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
